2dd91ecd19b4cd2621aa6e4d0a15efe65e187069
Chasen Stahl <stahlchasen@gmail.com>
2026-04-02 00:32:57 +0900

    Create curriculum.js

---
+++ curriculum.js
@@ -0,0 +1,6 @@
+// curriculum.js
+export const ITEM_DB = [
+    { id: 'k_ku', type: 'kanji', char: '区', meanings: ['ward', 'district'], readings: ['く'], meaningMnemonic: '...', readingMnemonic: '...' },
+    { id: 'k_yaku', type: 'kanji', char: '役', meanings: ['duty', 'service'], readings: ['やく'], meaningMnemonic: '...', readingMnemonic: '...' },
+    // ... all 8,000 items go here
+];
